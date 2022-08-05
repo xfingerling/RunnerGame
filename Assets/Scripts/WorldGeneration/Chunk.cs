f19a68b1b402a48dec3e8e4539b036ce6f036f56
Oleg Zavhorodnii <oleggenius95@gmail.com>
2022-08-05 17:54:14 +0300

    start dev

---
+++ Assets/Scripts/WorldGeneration/Chunk.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    [SerializeField] private float _chunkLength;
+
+    public Chunk ShowChunk()
+    {
+        gameObject.SetActive(true);
+        return this;
+    }
+
+    public Chunk HideChunk()
+    {
+        gameObject.SetActive(false);
+        return this;
+    }
+}

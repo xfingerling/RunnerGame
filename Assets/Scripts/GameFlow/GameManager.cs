2906f76a14fba7d8655d7693e1ab17e67c571aae
Oleg Zavhorodnii <oleggenius95@gmail.com>
2022-08-08 14:09:57 +0300

    game state

--- Assets/Scripts/GameFlow/GameManager.cs
+++ Assets/Scripts/GameFlow/GameManager.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get { return _instance; } }
+    private static GameManager _instance;
+
+    [SerializeField] private PlayerMotor _motor;
+
+    public PlayerMotor Motor => _motor;
+
+    private GameState _state;
+
+    private void Awake()
     {
-        
+        _instance = this;
+        _state = GetComponent<GameStateInit>();
+        _state.Construct();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _state.UpdateState();
+    }
+
+    public void ChangeState(GameState state)
+    {
+        state.Construct();
+        _state = state;
+        state.Destruct();
     }
 }
--- Assets/Scripts/GameFlow/GameState/GameState.cs
+++ Assets/Scripts/GameFlow/GameState/GameState.cs
@@ -9,7 +9,10 @@ public abstract class GameState : MonoBehaviour
         brain = GetComponent<GameManager>();
     }
 
-    public virtual void Construct() { }
+    public virtual void Construct()
+    {
+        Debug.Log($"Constructing {this.ToString()}");
+    }
     public virtual void Destruct() { }
     public virtual void UpdateState() { }
 }
